e242038192e41c7df01cd1b7647cf97e3ddef908
lofreitas2 <lofreitas2@STFJGRC026045-W.stefanini.dom>
2018-03-06 16:56:40 -0300

    Commit

--- Server/Fontes/src/main/java/com/jrpiassa/listatelefonica/ListaTelefonicaApiApplication.java
+++ Server/Fontes/src/main/java/com/jrpiassa/listatelefonica/ListaTelefonicaApiApplication.java
@@ -3,12 +3,19 @@ package com.jrpiassa.listatelefonica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ListaTelefonicaApiApplication {
+public class ListaTelefonicaApiApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListaTelefonicaApiApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ListaTelefonicaApiApplication.class);
 	}
 
 }
